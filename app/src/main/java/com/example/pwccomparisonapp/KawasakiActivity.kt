cc989a7977969e87ed74574e998652d67f393310
ChristianMRodas <chrismrodas@gmail.com>
2020-04-27 01:21:22 -0500

    Test 1

---
+++ app/src/main/java/com/example/pwccomparisonapp/KawasakiActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pwccomparisonapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KawasakiActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kawasaki)
+    }
+}
---
+++ app/src/main/java/com/example/pwccomparisonapp/SeadooActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pwccomparisonapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeadooActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seadoo)
+    }
+}
